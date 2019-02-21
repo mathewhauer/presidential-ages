80e35ecde6a05327e0e5cb35a9b314b02e556fa3
unknown <mehauer@fsu.edu>
2019-02-21 08:59:31 -0500

    testing Binder.

---
+++ install.R
@@ -0,0 +1,15 @@
+pkgs <- c(
+  "tidyverse",     # Tidyverse
+  "data.table",    # Data Management/Manipulation
+  "openxlsx",      # Microsoft Excel Files
+  "stringi",       #Character/String Editor
+  "stringr",       # Character/String Editor
+  "reshape2",      # Data Management/Manipulation
+  "scales",        # Number formatting
+  "cowplot",       # Plot Grids
+  "IDPmisc",       # Quality na.rm
+  "ggrepel",       # Repel Labels
+  "lemon"          # Axis manipulation
+)
+
+install.packages(pkgs)
